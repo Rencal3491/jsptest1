0986c628b81caaae10f584e53a06fbb17b940f63
MJMM <jmj3491329@naver.com>
2024-04-18 11:33:48 +0900

    create prj

---
+++ src/main/java/com/testjsp/testjsp1/MyServlet.java
@@ -0,0 +1,25 @@
+package com.testjsp.testjsp1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "myServlet", value = "/my")
+public class MyServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Myservlet</h1>");
+        out.println("</body></html>");
+    }
+
+
+
+}
---
+++ src/main/webapp/calc/input.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Rencal
+  Date: 2024-04-18
+  Time: 오전 11:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form>
+        <input type="number" name="n1">
+        <input type="number" name="n1">
+        <button type="submit">send</button>
+
+    </form>
+</body>
+</html>
